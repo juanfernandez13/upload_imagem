a61739218c1b85418246a103f735e88eb63f8404
Juan Pablo <juanp1310@outlook.com>
2022-10-03 09:59:03 -0300

    adicionando função DeleteImage no front

--- front_end_react/src/App.js
+++ front_end_react/src/App.js
@@ -6,6 +6,7 @@ import api from './config/configApi';
 function App() {
 
   const [image, setImage] = useState('');
+  const [imageDelete, setImageDelete] = useState('');
 
   const uploadImage = async e => {
     e.preventDefault();
@@ -32,16 +33,55 @@ function App() {
     })
   }
 
+  const DeleteImage = async (name) =>{
+    console.log("deleteImage");
+    const headers = {
+      'headers': {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const params = {
+      'filename':name
+    }
+
+    await api.delete("/delete-image",{data:params},headers)
+    .then((response) => {
+      console.log(response);
+    }).catch((err) => {
+
+      if(err.response){
+        console.log(err.response);
+      }
+      else{
+        console.log("Erro: Tente mais tarde!");
+      }
+
+    });
+
+  }
+
+  const handleChange = async (event) =>{
+    setImageDelete(event.target.value);
+  }
+
   return (
     <div className="App">
       <h1>Upload</h1>
       <form onSubmit={uploadImage}>
       <label htmlFor="">imagem: </label>
-      <input type="file" name="imagem" onChange={e => setImage(e.target.files[0])}/> <br /> <br />
-
-      {image && <img alt="imagem enviada" src={URL.createObjectURL(image)}/>}
+      <input type="file" name="imagem" onChange={e => setImage(e.target.files[0])}/> 
 
       <button type="submit">Salvar</button>
+      <br /><br />
+      {image && <img alt="imagem enviada" src={URL.createObjectURL(image)}/>}
+
+      </form>
+      <h1>Delete</h1>
+      <form onSubmit={e => e.preventDefault(e)}>
+          <label>Digite o nome da imagem: </label>
+          <input onChange={e => handleChange(e)} ></input>
+          <button type='submit' onClick={() => DeleteImage(imageDelete)}>deletar</button>
       </form>
     </div>
   );
